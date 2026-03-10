5cde329fff1b2b36493774baa4ea05a3db53ad94
yejin0718 <a89557400@gmail.com>
2026-03-10 11:17:25 +0900

    프로젝트 생성

---
+++ src/main/java/kr/ac/kopo/yejin/bookmarket/BookMarketApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.yejin.bookmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMarketApplication.class, args);
+    }
+
+}
